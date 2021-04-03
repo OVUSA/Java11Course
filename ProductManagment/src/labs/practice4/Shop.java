091b900c00435d4dab7f64329a9f37e0991e177e
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-04-03 15:08:02 -0500

    Shop.java
    add new products, practice casting and polymorphism

--- ProductManagment/src/labs/practice4/Shop.java
+++ ProductManagment/src/labs/practice4/Shop.java
@@ -4,16 +4,29 @@ import java.math.BigDecimal;
 
 public class Shop {
     public static void main(String [] args){
-        Product p1 = new Product(101,"Tea",BigDecimal.valueOf(1.99));
-        Product p2 = new Product(102,"Coffee",BigDecimal.valueOf(2.99), Rating.NOT_RATED);
-        Product p3 = new Product(103,"Cake",BigDecimal.valueOf(2.99));
-        Product p4 = new Product();
-        Product p5 = p3.applyRating(Rating.NOT_RATED);
-      //  p3 = p3.applyRating(Rating.NOT_RATED);
-        System.out.println(p1.getId()+" "+ p1.getName()+" "+p1.getPrice()+" available discount is "+ p1.getDiscount());
-        System.out.println(p2.getId()+" "+ p2.getName()+" "+p2.getPrice()+" available discount is "+ p2.getDiscount()+" "+p2.getRating());
-        System.out.println(p3.getId()+" "+ p3.getName()+" "+p3.getPrice()+" available discount is "+ p3.getDiscount());
-        System.out.println(p4.getId()+" "+ p4.getName()+" "+p4.getPrice()+" available discount is "+ p4.getDiscount());
-        System.out.println(p5.getId()+" "+ p5.getName()+" "+p5.getPrice()+" available discount is "+ p5.getDiscount());
+        ProductManager pm = new ProductManager();
+        Product p1 = pm.createProduct(101,"Tea",BigDecimal.valueOf(1.99),LocalDate.now().plusDays(1));
+        Product p2 = pm.createProduct(102,"Coffee",BigDecimal.valueOf(2.99), LocalDate.now());
+        Product p3 = pm.createProduct(103,"Cake",BigDecimal.valueOf(2.99),Rating.NOT_RATED, LocalDate.now().plusDays(2));
+        Product p4 = pm.createProduct(110, "Cookie,",BigDecimal.valueOf(4.99),Rating.NOT_RATED, LocalDate.now().plusDays(2));
+        Product p5 = pm.createProduct(104,"Chocolate",BigDecimal.valueOf(1.99),Rating.NOT_RATED,LocalDate.now().plusDays(2));
+        Product p6 = pm.createProduct(104,"Chocolate",BigDecimal.valueOf(10.99), LocalDate.now());
+        Product p7 = p5.applyRating(Rating.NOT_RATED);
+
+        p3.getBestBefore();
+
+        /*if (p3 instanceof Food){
+            LocalDate bestBefore = ((Food)p3).getBestBefore();
+
+        }*/
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+        System.out.println(p6);
+        System.out.println(p7);
+
     }
 }
