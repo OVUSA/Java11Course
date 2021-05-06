648c36dd0e64987d06188ccbc126efe6c0a45348
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-05 20:22:24 -0500

    added lock

--- Shopper.java
+++ Shopper.java
@@ -2,9 +2,11 @@ class Shopper extends Thread {
 	 
 	 static int cookieCount = 0;
 	  public void run() {
+		 pencil.lock();
 		  for ( int i =0; i < 10;i++) {
 			  cookieCount++;
-			  
+			  System.out.println(Thread.currentThread().getName()+ " is thinking");
+			  pencil.unlock();
 		  }
 	  }
 
