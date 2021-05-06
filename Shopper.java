99bbc0c7f8b9f54e927bb5ca3cea6a16f656c369
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-05 20:29:18 -0500

    rearranged  lock and unlock of threads

--- Shopper.java
+++ Shopper.java
@@ -2,13 +2,16 @@ class Shopper extends Thread {
 	 
 	 static int cookieCount = 0;
 	  public void run() {
-		 pencil.lock();
-		  for ( int i =0; i < 10;i++) {
+		 for ( int i =0; i < 5;i++) {
+			  pencil.lock();
 			  cookieCount++;
+			  pencil.unlock();
 			  System.out.println(Thread.currentThread().getName()+ " is thinking");
+			  try {
+			  Thread.sleep(500);
+			  }catch(InterruptedException e) {e.printStackTrace();}
 			 
 		  }
-		   pencil.unlock();
 	  }
 
 }
