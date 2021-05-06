36ac48090e07e8d4e81c1ed6f198fc8c46f1dd39
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-05-05 20:42:10 -0500

    ShopperSynchronized

--- Shopper.java
+++ Shopper.java
@@ -1,18 +1,21 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.*;
 class Shopper extends Thread {
 	 
 	 static int cookieCount = 0;
+	 private static synchronized void addCookie() {
+		cookieCount++;
+	 }
 	  public void run() {
-		 for ( int i =0; i < 5;i++) {
-			  pencil.lock();
-			  cookieCount++;
-			  pencil.unlock();
-			  System.out.println(Thread.currentThread().getName()+ " is thinking");
-			  try {
-			  Thread.sleep(500);
-			  }catch(InterruptedException e) {e.printStackTrace();}
-			 
+		 
+		  for ( int i =0; i < 5;i++) {
+			addCookie();
 		  }
+		 
+		 
 	  }
 
 }
+
+
 
